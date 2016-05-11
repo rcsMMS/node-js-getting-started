61833b29913ffa1ab2918db51a1f1f9e7497e35f
Ryan Spring <Ryan.Spring@t-systems-mms.com>
2016-05-11 15:21:10 +0200

    Fix forgotten line

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var pg = require('pg');
 var router = require(__dirname + '/models/routes.js')
 
 app.set('port', (process.env.PORT || 5000));
