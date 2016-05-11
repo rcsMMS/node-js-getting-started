203454ae4218a38bb7cb05c9ca271f4686f36614
Ryan Spring <Ryan.Spring@t-systems-mms.com>
2016-05-11 10:15:07 +0200

    Converted to rudimentry webservice

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+app.get('/:name',function(request,response) {
+  var obj = {message: {name: request.params.name,rank:'Corporal'}}
+
+  response.send(obj)
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
