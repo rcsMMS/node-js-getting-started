df842dec0c8623bb0bac358c42b5084940e61e99
Ryan Spring <Ryan.Spring@t-systems-mms.com>
2016-05-11 15:07:11 +0200

    Added postgres jazz

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ app.get('/',function(request,response) {
 
 app.use('/pizza',router)
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
 app.listen(app.get('port'), function() {
   //routes.setup(app)
   console.log('Node app is running on port', app.get('port'));
