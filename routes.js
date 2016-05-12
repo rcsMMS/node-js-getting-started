34bed75e6691bdf8d369a088de0177d5ee3a5265
Ryan Spring <Ryan.Spring@t-systems-mms.com>
2016-05-12 11:25:02 +0200

    iterating

--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 exports.setup = function(app,pizza) {
 
   // app.get('/pizza',pizza.pizzaCount);
-  var db = app.get('db')
-
-  console.log('Router DB ' + db);
-  app.get('/pizza',pizza.pizzaCount);
-  app.get('/pizza/:num',pizza.pizzaItem);
+  app.get('/pizza',function(req,res) {
+    var db = app.get('db')
+    console.log('Router DB' + db);
+    pizza.pizzaCnt(req,res);
+  })
+  app.get('/pizza/:num',function(req,res){
+    var db = app.get('db')
+    console.log('Router DB' + db);
+    pizza.pizzaItem(req,res);
+  });
 }
