57fea85222ea9c4b5db7d7de52cb09548fd4a253
Ryan Spring <Ryan.Spring@t-systems-mms.com>
2016-05-12 11:16:02 +0200

    Reorg

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 exports.setup = function(app,pizza) {
 
+  // app.get('/pizza',pizza.pizzaCount);
+  var db = app.get('db')
+
+  console.log('Router ' + db);
   app.get('/pizza',pizza.pizzaCount);
   app.get('/pizza/:num',pizza.pizzaItem);
 }
